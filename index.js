98af1cbea1d60250c8eaa63daf9b92da67d82ba6
Josie Cisneros <josiecisneros1224@gmail.com>
2023-09-21 05:35:54 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (distance) {
+    if (distance > 42) {
+        return distance - 42 
+    } else if (distance < 42) {
+        return 42 - distance
+    }
+}
+distanceFromHqInBlocks ()
+
+function distanceFromHqInFeet (distance) {
+    return distanceFromHqInBlocks(distance) * 264
+}
+distanceFromHqInFeet()
+
+function distanceTravelledInFeet (start, destination) {
+    if (destination > start) {
+        return (destination - start) * 264
+    } else if (destination < start) {
+        return (start -destination) *264
+    }
+}
+distanceTravelledInFeet()
+
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet (start, destination) <= 400) {
+        return 0
+    } else if (distanceTravelledInFeet (start, destination) >= 401 && distanceTravelledInFeet (start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) -400) * .02
+    } else if (distanceTravelledInFeet (start, destination) >= 2001 && distanceTravelledInFeet (start, destination) <=2500) {
+        return 25
+    } else if (distanceTravelledInFeet (start, destination) >= 2501) {
+        return 'cannot travel that far'
+    }
+}
